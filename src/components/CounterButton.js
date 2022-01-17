e7e6f871647339268d796214ed7a9919be5edc6b
Yusuf-Saad <yusufmsmsaad@gmail.com>
2022-01-17 12:58:49 -0600

    nothing

--- src/components/CounterButton.js
+++ src/components/CounterButton.js
@@ -1,13 +1,13 @@
-import React, { PureComponent } from 'react';
+import React, { Component } from 'react';
 
-class CounterButton extends PureComponent {
+class CounterButton extends Component {
   constructor() {
     super();
     this.state = {
       count: 0
     }
   }
-  
+
   shouldComponentUpdate(nextProps, nextState) {
     if (this.state.count !== nextState.count) {
       return true
@@ -21,7 +21,6 @@ class CounterButton extends PureComponent {
     })
   }
   render() {
-    console.log('CounterButton')
     return (
       <button color={this.props.color} onClick={this.updateCount}>
         Count: {this.state.count}
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,6 @@ import CounterButton from './CounterButton';
 
 class Header extends Component {
   render() {
-    console.log('Header')
     return (
     <div>
       <h1 className='f1'>Robofriends</h1>
