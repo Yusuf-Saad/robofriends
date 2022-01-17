fd13acf342fbf17521b717bcaca47d602f03b748
Yusuf-Saad <yusufmsmsaad@gmail.com>
2022-01-17 16:28:08 -0600

    adding serivce worker 2

--- src/index.js
+++ src/index.js
@@ -20,17 +20,4 @@ ReactDOM.render(
                 <Provider store={store}>
                    <App /> 
                 </Provider>, document.getElementById('root'));
-
-if ('serviceWorker' in navigator) {
-   window.addEventListener('load', function() {
-     navigator.serviceWorker.register('/sw.js').then(function(registration) {
-       // Registration was successful
-       console.log('ServiceWorker registration successful with scope: ', registration.scope);
-     }, function(err) {
-       // registration failed :(
-       console.log('ServiceWorker registration failed: ', err);
-     });
-   });
- }
-
 reportWebVitals();
