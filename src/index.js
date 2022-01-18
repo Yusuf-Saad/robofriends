ab5f2416f8b12451ec1b032224da9f4cdcae6a8b
Yusuf-Saad <yusufmsmsaad@gmail.com>
2022-01-18 08:45:10 -0600

    adding service worker 5

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import thunkMiddleware from 'redux-thunk';
 import './index.css';
 import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import { searchRobots, requestRobots } from './reducers';
 import 'tachyons';
 const logger  = createLogger();
@@ -19,4 +20,9 @@ ReactDOM.render(
                 <Provider store={store}>
                    <App /> 
                 </Provider>, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister();
 reportWebVitals();
