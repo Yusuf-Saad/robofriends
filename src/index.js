224f63844ece84800d5932a447c0395f6495ec21
Yusuf-Saad <yusufmsmsaad@gmail.com>
2022-01-19 10:18:41 -0600

    configuring pwa

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import thunkMiddleware from 'redux-thunk';
 import './index.css';
 import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
-import * as serviceWorker from './serviceWorker';
 import { searchRobots, requestRobots } from './reducers';
 import 'tachyons';
 const logger  = createLogger();
@@ -24,5 +23,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorker.register();
 reportWebVitals();
